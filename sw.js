0c05b22f057c305994054dc450db563b8023b220
muquew <muquew@outlook.com>
2026-05-10 06:53:52 +0000

    refactor: harden external i18n loading

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // 改了代码要来这里改缓存名字
-const CACHE_NAME = 'task-tracer-v2.25';
+const CACHE_NAME = 'task-tracer-v3.0';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
     './manifest.json',
-    './resources/en.json?v=1.2',
-    './resources/zh-CN.json?v=1.2',
+    './resources/en.json?v=1.3',
+    './resources/zh-CN.json?v=1.3',
     './fav/android-chrome-192x192.png',
     './fav/android-chrome-512x512.png'
 ];
