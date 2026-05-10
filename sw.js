517976cce4388699894b056886e0aa5571f889f8
muquew <muquew@outlook.com>
2026-05-10 12:51:44 +0000

    fix: clarify reminder and backup actions

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'task-tracer-v3.20';
+const CACHE_NAME = 'task-tracer-v3.21';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
     './manifest.json',
-    './resources/en.json?v=2.8',
-    './resources/zh-CN.json?v=2.8',
+    './resources/en.json?v=2.9',
+    './resources/zh-CN.json?v=2.9',
     './fav/android-chrome-192x192.png',
     './fav/android-chrome-512x512.png'
 ];
