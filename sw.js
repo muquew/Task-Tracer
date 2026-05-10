022f96ef94443d913bc1be67f7ac90a8195026be
muquew <muquew@outlook.com>
2026-05-10 13:51:59 +0000

    feat: add calendar quick task creation

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'task-tracer-v3.25';
+const CACHE_NAME = 'task-tracer-v3.26';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
     './manifest.json',
-    './resources/en.json?v=3.3',
-    './resources/zh-CN.json?v=3.3',
+    './resources/en.json?v=3.4',
+    './resources/zh-CN.json?v=3.4',
     './fav/android-chrome-192x192.png',
     './fav/android-chrome-512x512.png'
 ];
